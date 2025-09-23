151239b51d93be78b514f45dca917578915471a7
AmerBugshan <amer.s.bugshan@gmail.com>
2025-09-23 18:10:20 +0300

    create http module

---
+++ server.js
@@ -0,0 +1,15 @@
+import http from "node:http";
+
+
+const PORT = 8000;
+
+const server = http.createServer((req, res) => {
+
+
+  res.end("Hello From The Server!");
+
+
+});
+
+
+server.listen(PORT, () => console.log("Server running on port: 8000"));
